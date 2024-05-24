3327433d89d93f7e96e03d0897f896fa546641ff
Carolina Alejandra Banegas <carolina.banegas@yahoo.com.ar>
2024-05-24 15:14:45 +0000

    ya arreglé el EJERCICIO 7 . Me falta escribir el 8 que hice unas modificaciones. PERO YA se puede testear el EJERCICIO 7

--- SolucionCaro.hs
+++ SolucionCaro.hs
@@ -6,7 +6,7 @@ import Data.Char
 
 
 -- Completar!
--- Nombre de grupo: {}
+-- Nombre de grupo: {Exactamentes}
 -- Integrante1: { 27355966,Banegas Carolina Alejandra}
 -- Integrante2: { DNI2,apellidoYNombre2}
 -- Integrante3: { DNI3,apellidoYNombre3}
@@ -17,43 +17,35 @@ import Data.Char
 -- EJ 1
 esMinuscula :: Char -> Bool
 esMinuscula c = ord c >= ord 'a' && ord c <= ord 'z'
---OJO!! Revisar. Faltó en programado ALEJO, el menor o igual ord 'z'. Ver si queda mejor el ord 'a', que el 97. 
 
 -- EJ 2
 letraANatural :: Char -> Int
-letraANatural c = ord c - ord 'a'
---OK. IDENTICO a lo programado por ALEJO, excepto el ord 'a', que veremos si conviene dejarlo asi porque se lee mejor. 
+letraANatural c = ord c - (ord 'a')
+
+--EJ 3
 desplazar :: Char -> Int -> Char
 desplazar c 0 = c
 desplazar c n |not (esMinuscula c) = c
-              |letraANatural c + n >=0 && letraANatural c + n <= 25 = chr (ord c + n)
-              |n > 0 = chr (96 + mod (n - 25 + letraANatural c) 26)
-              |otherwise = chr (ord 'a'+ mod (26 + n + letraANatural c) 26)
---testeado con numeros grandes. Preparar mas casos especiales para testear 
+              |(letraANatural c) + n >=0 && (letraANatural c) + n <= 25 = chr(ord c + n)
+			  |n > 0 = chr(96 + (mod (n - 25 + letraANatural c) 26))
+			  |otherwise = chr((ord 'a')+ mod (26 + n + letraANatural c) 26)
+
 --EJ 4
 cifrar :: String -> Int -> String
 cifrar [] _ = []
 cifrar (s:ss) n |not (esMinuscula s) = s : cifrar ss n
-                |otherwise = desplazar s n:cifrar ss n
---OK. IDENTICO a lo programado por ALEJO. 
+                |otherwise = (desplazar s n):cifrar ss n
 
 -- EJ 5
 descifrar :: String -> Int -> String
 descifrar [] _ = []
 descifrar s n = cifrar s (-n)
---OK. IDENTICO a lo programado por ALEJO
 
 
 cifrarListaAux :: [String] -> Int -> [String]
 cifrarListaAux [] _ = []
 cifrarListaAux (palabra:ls) n = cifrar palabra (n-(length ls + 1)): cifrarListaAux ls n
 
---version ALEJO: tiene un calculo menos la de Alejo. Conviene usar la suya.
---cifrarListaAux :: [String] -> Int -> String 
---cifrarListaAux [] _ = []
---cifrarListaAux (s:ss) n = (cifrar s n) : (cifrarListaAux ss (n + 1))
---cifrarLista :: [String]->[String]
---cifrarLista x = cifrarListaAux x 0
 
 -- EJ 6
 cifrarLista :: [String] -> [String]
@@ -63,102 +55,120 @@ cifrarLista (palabra:ls) = cifrarListaAux (palabra:ls) (length (palabra:ls))
 
 cantidadVecesLetra :: Char -> String -> Int
 cantidadVecesLetra _ [] = 0
-cantidadVecesLetra c (x:xs) |c == x = 1 + cantidadVecesLetra c xs
-                            |otherwise = cantidadVecesLetra c xs
+cantidadVecesLetra c (x:xs) |c == x = 1 + (cantidadVecesLetra c xs)
+                            |otherwise = (cantidadVecesLetra c xs)
 
 cantidadMinusculas :: String -> Int
 cantidadMinusculas [] = 0
 cantidadMinusculas (c:ls) |esMinuscula c = 1 + cantidadMinusculas ls
                           |otherwise = cantidadMinusculas ls
+						  
+dividir :: Int -> Int -> Float
+dividir a b = fromIntegral a / fromIntegral b
 
---porcentajeFrecuencia :: Char -> String -> Int -> Float
---porcentajeFrecuencia c s n |n == 0 = 0.00
- --                          |otherwise = (cantidadVecesLetra c s)/n * 100
+porcentajeFrecuencia :: Char -> String -> Int -> Float
+porcentajeFrecuencia c s n |(n == 0 ) = 0
+                           |otherwise = (dividir (cantidadVecesLetra c s) n )* 100
 
-porcentajeFrecuencia :: Char -> String -> Int -> Int
-porcentajeFrecuencia c s n |n == 0 = 0
-                           |otherwise = cantidadVecesLetra c s * 100
--- OJO!! me falta agregar la division por n (cantidadMinusculas s) pero tengo que ver el tema float de agregar a frecuencia, frecuenciaAux 
---y porcentajeFrecuencia
-frecuenciaAux :: Char -> String -> [Int]
-frecuenciaAux 'z'_ = []
-frecuenciaAux c s = porcentajeFrecuencia c s (cantidadMinusculas s) : frecuenciaAux  (desplazar c 1) s
-
+frecuenciaAux :: Char -> String -> [Float]
+frecuenciaAux 'z' s = [porcentajeFrecuencia 'z' s (cantidadMinusculas s)]
+frecuenciaAux c s = ( porcentajeFrecuencia c s (cantidadMinusculas s) : frecuenciaAux  (desplazar c 1) s )
+                           
 -- EJ 7
-frecuencia :: String -> [Int]
-frecuencia [] = []
-frecuencia (s:ls )= frecuenciaAux 'a' (s:ls)
+frecuencia :: String -> [Float]
+frecuencia s = frecuenciaAux 'a' s 
+
+--EJ 8
 
 --cifradoMasFrecuenteAux :: [Float]-> (Char, Float)
 --cifradoMasFrecuenteAux [] = ('a',0.0)
 --cifradoMasFrecuenteAux (x:y:xs)|x>y = ( posicion x ,x)
 --                               |otherwise = y: cifradoMasFrecuenteAux xs
---ver posicion relativa y dato maximo y caso base de requerirse  PEDIENTE _____________REVEERRRRRRRRRR
+--ver posicion relativa y dato maximo y caso base de requerirse
 
 
-esDescifrado:: String -> String -> Bool
+--EJ 9
+esDescifrado:: String -> String -> Bool				 
 esDescifrado s1 s2 = esDescifradoAux s1 s2 0
 
 esDescifradoAux :: String -> String -> Int -> Bool
 esDescifradoAux _ _ 27 = False
-esDescifradoAux s1 s2 n | s2 == cifrar s1 n= True
+esDescifradoAux s1 s2 n | (s2 == (cifrar s1 n) )= True
                         |otherwise = esDescifradoAux s1 s2 (n+1)
 
 
 todosLosDescifradosAux :: String -> [String] -> [(String,String)]
 todosLosDescifradosAux _ [] = []
-todosLosDescifradosAux s ls |esDescifrado s (head ls) && s /= head ls = (s,head ls): (head ls,s): todosLosDescifradosAux s (tail ls)
+todosLosDescifradosAux s ls |(esDescifrado s (head ls)) && (s /= head ls) = (s,head ls): (head ls,s): todosLosDescifradosAux s (tail ls)
                             |otherwise = todosLosDescifradosAux s (tail ls)
 -- EJ 10
 todosLosDescifrados :: [String] -> [(String, String)]
 todosLosDescifrados [] = []
-todosLosDescifrados ls = todosLosDescifradosAux (head ls) (tail ls) ++ todosLosDescifrados (tail ls)
+todosLosDescifrados ls = (todosLosDescifradosAux (head ls) (tail ls)) ++ todosLosDescifrados (tail ls)
 
 
+agregarAClave :: String -> Int -> String
+agregarAClave _ 0 = []
+agregarAClave s n = (head s ): agregarAClave  (tail s) (n-1) 
 
---AgregarAClave :: String -> Int -> String
---AgregarAClave _ 0 = []
---AgregarAClave s n = head s : AgregarAClave  (tail s ) (n-1)
+repetirClave :: String -> Int -> String
+repetirClave _ 0 = []
+repetirClave s n = s ++ repetirClave s (n-1)
 
 -- EJ 11
---expandirClave :: String -> Int -> String
---expandirClave s n =  s ++ agregarAClave s (n - length s + 1)
+expandirClave :: String -> Int -> String
+expandirClave s n | (n < length s ) = (agregarAClave s n )
+                  |otherwise = (repetirClave s (div n (length s)) ) ++ (  agregarAClave s (mod n (length s))  ) 
 
--- EJ 12
---cifrarVigenere :: String -> String -> String
---cifrarVigenere [] [] = []
---cifrarVigenere (c:ls) clave = (desplazar c (letraANatural (head (expandirClave clave))) : cifrarVigenere ls (tail expandir (clave))
+
+cifrarVigenere :: String -> String -> String
+cifrarVigenere [] _ = []
+cifrarVigenere (x:xs) (c:cs) =(desplazar x (letraANatural c)): cifrarVigenere xs (tail (expandirClave (c:cs) (length (x:xs))))
 
 
 -- EJ 13
---descifrarVigenere :: String -> String -> String
---descifrarVigenere [] [] = []
---descifrarVigenere (c:ls) clave = (desplazar c ( - letraANatural (head (espandirClave clave))) : cifrarVigenere ls (tail clave)
+descifrarVigenere :: String -> String -> String
+descifrarVigenere [] [] = []
+descifrarVigenere (x:xs) (c:cs) = (desplazar x (-(letraANatural c))):(descifrarVigenere xs (tail (expandirClave (c:cs) (length (x:xs)))))
 
---listarClavesExpandidas :: Int -> [String]
---listarClavesExpandidas n (clave:ls) = (expandirClave clave n) : listarClavesExpandidas ls
+absoluto :: Int -> Int
+absoluto x | x < 0 = - x 
+           |otherwise = x
 
---distanciaCifrado :: String -> String -> (
---distanciaCifrado (x:s1) (y:s2) = modulo ((letraANatural x)- (letraANatural y)) + distanciaCifrado s1
+distanciaCifrado :: String -> String -> Int
+distanciaCifrado [] _ = 0
+distanciaCifrado (x:s1) (y:s2) = absoluto ((letraANatural x) - (letraANatural y)) + distanciaCifrado s1 s2
 
---listarDistanciaCifrado :: String -> [String] -> [(String,String)]
---listarDistanciaCifrado s (clave:ls) = (clave, distanciaCifrado s clave)  : listarDistanciaCifrado s ls      
+listarCifradoV :: String -> [String] -> [(String,String)]
+listarCifradoV _ [] = []
+listarCifradoV s (c:cs) = ( cifrarVigenere s (expandirClave c (length s)),c) : (listarCifradoV s cs)      
+ 
+listarDistanciaCifrado :: String -> [(String,String)]-> [(String,Int)]
+listarDistanciaCifrado _ [] = []                                          
+listarDistanciaCifrado s (c:cs)= (snd(c), distanciaCifrado (fst(c)) s ): (listarDistanciaCifrado s cs)
 
---menorDistanciaCifrado :: [(String, String)]-> [String,String]
---menorDistanciaCifrado [] = []
---menorDistanciaCifrado ((clave1,d1):(clave2,d2):ls) | d1 < d2 = (clave1,d1) : menorDistanciaCifrado ls
---                                                   |otherwise = (clave2,d2) : menorDistanciaCifrado ls
+menorDistanciaCifradoV :: [(String,Int)] -> [(String,Int)]
+menorDistanciaCifradoV [] = []
+menorDistanciaCifradoV (c1:c2:cs) | ( snd(c1) < snd(c2) ) = c1 : (menorDistanciaCifradoV cs)
+                                  | otherwise = c2 : (menorDistanciaCifradoV cs)
 
 -- EJ 14
---peorCifrado :: String -> [String] -> String
---peorCifrado _ [] = []
---peorCifrado s ls = fst (head (menorDistanciaCifrado (ListarDistanciaCifrado s (listarClavesExpandidas  (length s) ls ))))
+peorCifrado :: String -> [String] -> String
+peorCifrado _ [] = []
+peorCifrado s cs = fst(head (menorDistanciaCifradoV (listarDistanciaCifrado s (listarCifradoV s cs))))
 
---funcion a tener en cuenta si necesito:
---absoluto :: Int -> Int -> Int
---absoluto x | x<0 = -x
---           |otherwise = x
+  
+listarPosiblesDescifrados :: [String] -> String -> [(String,String)]
+listarPosiblesDescifrados [] _ = []
+listarPosiblesDescifrados (c:cs) s = (descifrarVigenere s (expandirClave c (length s)),c): (listarPosiblesDescifrados cs s)
 
+listarMensajes :: [(String,String)] -> String-> [(String,String)]
+listarMensajes [] _ =[]
+listarMensajes (d:dc) m | ((fst (d)) == m) = (m,snd(d)) : (listarMensajes dc m)
+                        | otherwise = (listarMensajes dc m)
+ 
+						 							 
 -- EJ 15
---combinacionesVigenere :: [String] -> [String] -> String -> [(String, String)]
---combinacionesVigenere _ _ _ = [("hola", "b")]
+combinacionesVigenere :: [String] -> [String] -> String -> [(String,String)]
+combinacionesVigenere [] _ _ = []
+combinacionesVigenere (m:ms) cs s = (listarMensajes (listarPosiblesDescifrados cs s) m) ++ (combinacionesVigenere ms cs s)
